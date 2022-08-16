63c61353bbe212472aab64400bed13e75e2efece
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-16 22:43:06 +0530

    in-built string functions

---
+++ in-built string.js
@@ -0,0 +1,26 @@
+var x = 100;
+var y = 200;
+   
+var z1 = x+y;
+var z2 = x.toString() + y;
+   
+console.log(z1); 
+console.log(z2);
+
+
+
+
+
+var x = "some ";
+var y = "string";
+   
+console.log(x.concat(y));
+
+
+
+
+
+var x = "some";
+var y = "string";
+   
+console.log(x+y);
